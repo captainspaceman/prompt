2a8322d4825b51994d3eb2c9139ee3f114afd57b
ryan@cartoonfood.com <ryan.michael.farrell@gmail.com>
2018-05-23 01:09:49 -0700

    added a separate jquery js file

--- public/app/app.js
+++ public/app/app.js
@@ -12,39 +12,7 @@
 angular
 .module('app', ['ui.router','app.home','firebase'])
 .config(function($urlRouterProvider){
-	console.log('can you hear me?')
+	console.log('app.js is loading')
   $urlRouterProvider.otherwise('/');
 })
 
-
-  $("#submitButton").click(function(){
-      console.log('button was clicked');
-      // location.reload();
- 
-  });
-
-$('#r').click(function(){
-  $('#r').click(function(event){
-    event.preventDefault();
-  })
-  console.log('change view button clicked');
-  $('body').toggleClass('black');
-  $('p').toggleClass('white');
-  $('h1').toggleClass('white');
-})
-
-
-
-  //social share buttons
-
-          $("#facebook-share").click(function(){
-            console.log("Facebook share button clicked");
-          })
-
-          $("#twitter-share").click(function(){
-            console.log("Twitter share button clicked");
-          })
-
-          $("#email-share").click(function(){
-            console.log("Email share button clicked");
-          })
